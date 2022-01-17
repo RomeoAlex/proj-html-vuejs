ecd81dbb5a47cd9294876b0622862109263a5f47
RomeoAlex <romeo.alessandro17@gmail.com>
2022-01-17 15:59:06 +0100

    importato immagini creato jumbotron

---
+++ src/components/Jumbotron.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="jumbotronw-rapper">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.jumbotronw-rapper{
+    width:100%;
+    background-image: url('../images/home-background-hero-scaled.jpg');
+    background-size: 100%;
+    background-repeat: no-repeat;
+    // test
+    height: 1500px;
+}
+</style>
